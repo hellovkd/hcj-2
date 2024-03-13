7cac126f85bc8f5ec2fc86c968873df93401c1ab
hellovkd <soojooo123@naver.com>
2024-03-13 12:18:12 +0900

    2nd

--- main.js
+++ main.js
@@ -14,17 +14,17 @@ function linen() {
 }
 
 function showhtml() {
-    document.getElementById("fig").src = "img/html5.jpg"
+    document.getElementById("fig").src = "img/html.png"
     document.getElementById("desc").innerHTML =
         "<b>HTML5</b>는 구조적 웹문서를 작성하는데 사용하는 마크업 언어이다."
 }
 function showcss() {
-    document.getElementById("fig").src = "img/css3.jpg"
+    document.getElementById("fig").src = "img/css.png"
     document.getElementById("desc").innerHTML =
         "<b>CSS3</b>는 HTML 문서의 스타일을 지정하기 위한 언어이다."
 }
 function showjs() {
-    document.getElementById("fig").src = "img/javascript.jpg"
+    document.getElementById("fig").src = "img/js.jpg"
     document.getElementById("desc").innerHTML =
         "<b>Javascript</b>는 웹을 위한 프로그래밍 언어이며 웹문서에서 동작을 실행할 수 있게 한다."
 }
